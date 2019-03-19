f9fa3cfee42c5fd7a54ca8d76ff01cbd9ef4716c
Daniel Shotonwa <danielshotonwa53@gmail.com>
2019-03-19 22:54:12 +0100

    set up graphQL server 🍴

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log("I am in")
+import { GraphQLServer } from 'graphql-yoga'
+
+// type definition
+const typeDefs = `
+  type Query {
+    hello: String!
+  }
+`
+
+// resolver
+const resolvers = {
+  Query: {
+    hello() {
+      return 'This is my first query'
+    }
+  }
+}
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+});
+
+server.start(() => {
+  console.log("The server has started");
+});
