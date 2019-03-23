1b0e0212a54bb8faa9fc0f770cce02e5e88ace0c
Daniel Shotonwa <danielshotonwa53@gmail.com>
2019-03-23 17:08:14 +0100

    set up relationship for comments, users and posts🎉

--- src/index.js
+++ src/index.js
@@ -5,24 +5,47 @@ const users = [{
   name: 'Tolu',
   email: 'dan@yahoo.com',
   age: 6
+}, {
+  id:2,
+  name: 'Daniel',
+  email: "dan@emaail.com",
+  age: 7
 }]
 
 const posts = [{
   id: 1,
   title: "Going home",
   body: "Home sweet home",
-  published: false
+  published: false,
+  authorId: 1,
+  commentId: 1
 }, {
   id: 2,
   title: "peculiar",
   body: "peculiar is precious to me",
-  published: true
+  published: true,
+  authorId: 1,
+  commentId: 1
 }
 ]
+
+const comments = [{
+  id: 1,
+  text: '33j3',
+  authorId: 1,
+  postId: 1
+},
+  {
+    id: 2,
+    text: 'I am home',
+    authorId: 2,
+    postId: 2
+  }]
 // scaler types: String, Int, Float, Boolean, ID
 // type definition
 const typeDefs = `
   type Query {
+    comments: [Comment!]!
     users(query: String): [User!]!
     posts(query: String): [Post!]!
     me: User!
@@ -34,6 +57,8 @@ const typeDefs = `
     name: String!
     email: String!
     age: Int
+    posts: [Post!]!
+    comments: [Comment!]!
   }
 
   type Post {
@@ -41,12 +66,25 @@ const typeDefs = `
     title: String!
     body: String!
     publised: Boolean!
+    author: User!
+    comments: [Comment!]!
+  }
+
+  type Comment {
+    id: ID!
+    text: String!
+    author: User!
+    post: Post!
   }
 `
 
 // resolver
 const resolvers = {
   Query: {
+    comments(parent, args, ctx, info) {
+      return comments
+    },
+
     posts(parent, args, ctx, info) {
       if (!args.query) {
         return posts;
@@ -57,6 +95,7 @@ const resolvers = {
         return isBody || isTitle
       })
     },
+
     users(parent, args, ctx, info) {
       if (!args.query) {
         return users 
@@ -65,6 +104,7 @@ const resolvers = {
         return x.name.toLowerCase().includes(args.query.toLowerCase());
       });
     },
+
     me() {
       return {
         id: '78',
@@ -73,13 +113,47 @@ const resolvers = {
         age: 28
       }
     },
-    post() {
-      return {
-        id: '7888',
-        title: 'ejdhhdhd',
-        body: 'I am ping',
-        publised: true
-      }
+  },
+
+  Post: {
+    author(parent, args, ctx, info) {
+      return users.find((user) => {
+        return user.id === parent.authorId
+      })
+    },
+
+    comments(parent, args, ctx, info) {
+      return comments.filter((comment) => {
+        return comment.postId == parent.id 
+      })
+    }
+  },
+
+  User: {
+    posts(parent, args, ctx, info) {
+      return posts.filter((post) => {
+        return post.authorId === parent.id
+      });
+    },
+
+    comments(parent, args, ctx, info) {
+      return comments.filter((comment) => {
+        return comment.authorId == parent.id
+      })
+    }
+  },
+
+  Comment: {
+    author(parent, args, ctx, info) {
+      return users.find((user) => {
+        return user.id === parent.authorId
+      })
+    },
+
+    post(parent, args, ctx, info) {
+      return posts.find((post) => {
+        return parent.id === post.commentId
+      })
     }
   }
 }
