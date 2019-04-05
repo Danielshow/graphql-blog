d5a9628f20bfe1d7d9777e00ec8e773ef356d277
Daniel Shotonwa <danielshotonwa53@gmail.com>
2019-04-05 23:57:57 +0100

    Add subscription with Graphql

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,20 +1,26 @@
 const Subscription = {
   comment: {
-    subscribe(parent, args, {db, pubsub}, info) {
-      const { postId } = args
-      const post = db.posts.find((post) => post.id == postId && post.published)
-
-      if (!post) {
-        throw new Error("Post not found")
-      }
-
-      return pubsub.asyncIterator(`comment ${postId}`)
-    }
+    subscribe(parent, { post }, { prisma }, info) {
+      return prisma.subscription.comment({
+        where: {
+          node: {
+            post: {
+              id: post
+            }
+          }
+        }
+      }, info) 
+    },
   },
-
   post: {
-    subscribe(parent, args, { pubsub }, info) {
-      return pubsub.asyncIterator("post")
+    subscribe(parent, args, { prisma }, info) {
+      return prisma.subscription.post({
+        where: {
+          node: {
+            published: true
+          }
+        }
+      }, info)
     }
   }
 }
