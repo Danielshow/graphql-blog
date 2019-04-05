e8f3117e599e031c9ef3eed0aeafc2a22e8ac8ce
Daniel Shotonwa <danielshotonwa53@gmail.com>
2019-04-06 00:27:49 +0100

    secure server graphql playground

--- src/prisma.js
+++ src/prisma.js
@@ -1,24 +1,10 @@
-import { Prisma } from 'prisma-binding'
+imp<Down>ort { Prisma } from 'prisma-binding'
 
 const prisma = new Prisma({
   typeDefs: "src/generated/prisma.graphql",
-  endpoint: "http://localhost:4466"
+  endpoint: "http://localhost:4466",
+  secret: "mysecrettext"
 })
 
 export default prisma
-
-//const updatePostForUser = async (postId, data) => {
-//  const isPostExist = await prima.exists.Post({id: postId})
-//  if (!isPostExist) {
-//    throw new Error("Post not found")
-//  }
-//  const post = await prisma.mutation.updatePost({
-//    where: {
-//      id: postId
-//    },
-//    data
-//  }, '{author {id name email posts {id title published}}}');
-//
-//  return post.author
-//}
 
