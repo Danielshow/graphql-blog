80e6cc6a5139e5a8bc2b7b7c72db253d3d37eed5
Daniel Shotonwa <danielshotonwa53@gmail.com>
2019-04-29 00:25:33 +0100

    clean up code

--- src/utils/getUserId.js
+++ src/utils/getUserId.js
@@ -4,7 +4,7 @@ const getUserId = (request, requireAuth=true) => {
   const headers = request.request ? request.request.headers.authorization : request.connecttion.context.Authorization
   if (headers) {
     const token = headers.split(' ')[1]
-    const decoded = jwt.verify(token, 'thisisasecret') 
+    const decoded = jwt.verify(token, process.env.JWT_SECRET) 
     return decoded.userId
   }
   if (requireAuth) {
